62fceb7b6d5c121a0becc14a45cf1e761d8fc51a
marwangalal746 <marawangalal84@gmail.com>
2022-03-11 17:13:37 +0200

    adding constraints to User table

--- user service/src/main/java/com/DExam/User_Service/resources/modules/User.java
+++ user service/src/main/java/com/DExam/User_Service/resources/modules/User.java
@@ -1,10 +1,12 @@
 package com.DExam.User_Service.resources.modules;
 
-import lombok.;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.;
+import javax.persistence.*;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
 import java.util.Date;
 
 @Entity
@@ -13,11 +15,12 @@ import java.util.Date;
 public class User {
     private  @Id @GeneratedValue @Setter(AccessLevel.PROTECTED) long id;
     private String name;
+    @Column(unique=true)
     private String email;
+    @Column(unique=true)
     private String nationalID;
     private String password;
     private String img;
-    private Role role;
     private @Setter(AccessLevel.PROTECTED) String createdAt;
 
     public User(String name, String email, String nationalID, String password, String img, Role role) {
@@ -26,7 +29,6 @@ public class User {
         this.nationalID = nationalID;
         this.password = password;
         this.img = img;
-        this.role = role;
         SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
         Date date = new Date(System.currentTimeMillis());
         System.out.println(formatter.format(date));
