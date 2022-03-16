01853cecafb140dc1843671eef68f77639eaf27b
Eyad Youssef <eyadyoussef1336@gmail.com>
2022-03-16 18:50:55 +0200

    removed unnecessary code

--- user service/src/main/java/com/DExam/User_Service/utility/ResponseMessages.java
+++ user service/src/main/java/com/DExam/User_Service/utility/ResponseMessages.java
@@ -1,9 +1,9 @@
 package com.DExam.User_Service.utility;
 
-public final class ResponseMessages {
-    public static final String EMAIL_USED = "This email is already used";
-    public static final String NATIONAL_ID_USED = "This national ID is already used";
-    public static final String EMAIL_VERIFICATION = "Verify your email to finish the process using the verification code: ";
-    public static final String USER_NOT_FOUND = "User does not exist";
-    public static final String INVALID_EMAIL_PASSWORD = "Invalid email or password";
+public interface ResponseMessages {
+    String EMAIL_USED = "This email is already used";
+    String NATIONAL_ID_USED = "This national ID is already used";
+    String EMAIL_VERIFICATION = "Verify your email to finish the process using the verification code: ";
+    String USER_NOT_FOUND = "User does not exist";
+    String INVALID_EMAIL_PASSWORD = "Invalid email or password";
 }
