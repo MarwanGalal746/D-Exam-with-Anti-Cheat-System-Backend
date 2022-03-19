a379f5f1418ee0809a351c8af35466b13d127d36
marwangalal746 <marawangalal84@gmail.com>
2022-03-19 14:05:07 +0200

    unit test to findByEmail function in UserRepository

--- user service/src/test/java/com/DExam/User_Service/database/UserRepositoryTest.java
+++ user service/src/test/java/com/DExam/User_Service/database/UserRepositoryTest.java
@@ -1,9 +1,26 @@
 package com.DExam.User_Service.database;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.DExam.User_Service.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
 
 class UserRepositoryTest {
+    @Autowired
+    private UserRepository underTest;
 
-
-
+    @Test
+    void findByEmailTest() {
+        User user = new User();
+        String email = "eyaaad@gmail.com";
+        user.setEmail(email);
+        Optional exist = underTest.findByEmail(email);
+        assertThat(exist).isEqualTo(Optional.empty());
+    }
 }
