97cbdd2473fe2469599d1f0ce01f80c638433342
marwangalal746 <marawangalal84@gmail.com>
2022-03-20 12:25:15 +0200

    findByEmailTest using h2 database

--- user service/src/test/java/com/DExam/User_Service/database/UserRepositoryTest.java
+++ user service/src/test/java/com/DExam/User_Service/database/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -17,10 +18,22 @@ class UserRepositoryTest {
 
     @Test
     void findByEmailTest() {
-        User user = new User();
-        String email = "eyaaad@gmail.com";
-        user.setEmail(email);
-        Optional exist = underTest.findByEmail(email);
-        assertThat(exist).isEqualTo(Optional.empty());
+        String[] emails = new String[] { "eyad@gmail.com", "marwan@gmail.com", "ammar@gmail.com", "bodda@gmail.com"};
+        ArrayList<User> users = new ArrayList<>();
+        for (int i=0 ; i< emails.length-1 ; i++) {
+            User user  = new User();
+            user.setEmail(emails[i]);
+            underTest.save(user);
+            users.add(user);
+        }
+
+        for (int i=0 ; i<emails.length ; i++) {
+            Optional<User> exist = underTest.findByEmail(emails[i]);
+            if (i != emails.length-1) {
+                assertThat(exist).isNotEqualTo(Optional.empty());
+                continue;
+            }
+            assertThat(exist).isEqualTo(Optional.empty());
+        }
     }
 }
