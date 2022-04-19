59cd4bfc4a243d319aa895cb26616c48bb7c0b76
marwangalal746 <marawangalal84@gmail.com>
2022-04-19 14:46:34 +0200

    add exam service

---
+++ exam service/pkg/services/examService.go
@@ -0,0 +1,24 @@
+package services
+
+import "exam_service/pkg/domain/exam"
+
+type ExamService interface {
+	Create(exam.Exam) (int, error)
+	Read() error
+}
+
+type DefaultExamService struct {
+	repo exam.ExamRepository
+}
+
+func (e DefaultExamService) Create(newExam exam.Exam) error {
+	return e.repo.Create(newExam)
+}
+
+func (e DefaultExamService) Read() error {
+	return e.repo.Read()
+}
+
+func NewExamService(repository exam.ExamRepository) DefaultExamService {
+	return DefaultExamService{repo: repository}
+}
