48f418a3b886619afcc60ce0093216d41423ff0c
marwangalal746 <marawangalal84@gmail.com>
2022-04-21 02:07:04 +0200

    removing some errors

--- exam service/pkg/errs/errors.go
+++ exam service/pkg/errs/errors.go
@@ -3,26 +3,10 @@ package errs
 import "errors"
 
 var (
-	ErrDb                      = errors.New("unexpected database error")
-	ErrUnmarshallingJson       = errors.New("can't unmarshal the JSON to instance")
-	ErrMarshallingInstance     = errors.New("can't marshal the instance to JSON")
-	ErrNoRowsFound             = errors.New("no values found")
-	ErrServerErr               = errors.New("internal server error")
-	ErrCannotDelete            = errors.New("business Manager can't be deleted")
-	ErrCannotUpdate            = errors.New("business Manager can't be upated")
-	ErrInvalidPassword         = errors.New("invalid password")
-	ErrInvalidToken            = errors.New("invalid password")
-	ErrDuplicateExam           = errors.New("an exam with this name exists")
-	ErrDrinkNotFound           = errors.New("this drink is not found")
-	ErrDuplicateDrinkForUser   = errors.New("the user has this drink already")
-	ErrDessertNotFound         = errors.New("this dessert is not found")
-	ErrDuplicateDessertForUser = errors.New("the user has this dessert already")
-	ErrNoDrinksFound           = errors.New("no drinks found")
-	ErrNoDessertsFound         = errors.New("no desserts found")
-	ErrEmailMissing            = errors.New("email is missing")
-	ErrInvalidEmail            = errors.New("invalid email")
-	ErrNoSandwichesFound       = errors.New("no sandwiches found")
-	ErrSandwichNotFound        = errors.New("this sandwich is not found")
+	ErrDb                  = errors.New("unexpected database error")
+	ErrUnmarshallingJson   = errors.New("can't unmarshal the JSON to instance")
+	ErrMarshallingInstance = errors.New("can't marshal the instance to JSON")
+	ErrDuplicateExam       = errors.New("an exam with this name exists")
 )
 
 type Response struct {
