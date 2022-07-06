0a0d2c20bdc00f9bb5935af659522a46e52949f8
Eyad Youssef <eyadyoussef1336@gmail.com>
2022-07-06 18:19:55 +0200

    updated token generator method

--- src/main/java/com/DExam/User_Service/config/JwtManager.java
+++ src/main/java/com/DExam/User_Service/config/JwtManager.java
@@ -1,55 +1,22 @@
 package com.DExam.User_Service.config;
 
-import com.DExam.User_Service.domain.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
 @Service
 public class JwtManager {
-
-    private final String secret = "Thisismytestprivatekey";
-
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    public String generateToken(String email, String role) {
-        Map<String, Object> claims = new HashMap<>();
+    private static final String SECRET_KEY = System.getenv("AUTH_SECRET_KEY");
+    static byte[] secretKey = SECRET_KEY.getBytes();
+    public static String generateToken(String email, String role) {
+        Claims claims = Jwts.claims().setSubject(email);
         claims.put("UserRole", role);
-        return createToken(claims, email);
-    }
-
-    private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, secret).compact();
-    }
-
-    public boolean validateToken(String token, User userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getEmail()) && !isTokenExpired(token));
+        long expTime = System.currentTimeMillis() + 1000 * 60 * 60 * 10;
+        Date exp = new Date(expTime);
+        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
 }
