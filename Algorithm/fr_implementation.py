66668cab03e3a3683746854c21cdd722af80a64d
Eyad Youssef <eyadyoussef1336@gmail.com>
2022-07-14 20:26:48 +0200

    updated algorithm implementation

--- Algorithm/fr_implementation.py
+++ Algorithm/fr_implementation.py
@@ -1,12 +1,22 @@
+import cv2
 import face_recognition
 
 
 def run(img1, img2):
-    image1 = face_recognition.load_image_file(img1)
-    image2 = face_recognition.load_image_file(img2)
+    image = face_recognition.load_image_file(img1)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    image1_encoding = face_recognition.face_encodings(image1)[0]
-    image2_encoding = face_recognition.face_encodings(image2)[0]
+    boxes = face_recognition.face_locations(rgb)[0]
+    encoding1 = face_recognition.face_encodings(rgb)[0]
 
-    results = face_recognition.compare_faces([image1_encoding], image2_encoding)
-    return results
+    # read 2nd image and store encodings
+    image = face_recognition.load_image_file(img2)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    boxes = face_recognition.face_locations(rgb)[0]
+    encoding2 = face_recognition.face_encodings(rgb)[0]
+
+    # now you can compare two encodings
+    # optionally you can pass threshold, by default it is 0.6
+    matches = face_recognition.compare_faces([encoding1], encoding2)
+    return matches
