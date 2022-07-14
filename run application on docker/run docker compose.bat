b6736df880f2d7a13dafe75d8e12f0db15b5b41b
Eyad Youssef <eyadyoussef1336@gmail.com>
2022-07-14 20:45:07 +0200

    updated docker compose to contain fr service

--- run application on docker/run docker compose.bat
+++ run application on docker/run docker compose.bat
@@ -3,4 +3,5 @@ docker pull eyadzz/dexam_user_service:latest &
 docker pull eyadzz/dexam_course_service:latest &
 docker pull eyadzz/dexam_api_gateway:latest &
 docker pull eyadzz/dexam_overseer_service:latest &
+docker pull eyadzz/dexam_fr_service:latest &
 docker-compose -f backend.yaml up --build
