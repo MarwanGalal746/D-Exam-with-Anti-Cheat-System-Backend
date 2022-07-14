5fd4e9c2e02b46c956c48d4a7cb3e45a91f941dd
Marwan Galal <marawangalal84@gmail.com>
2022-07-14 21:34:48 +0200

    init

--- run application on docker/run docker compose.bat
+++
@@ -1,6 +0,0 @@
-docker pull eyadzz/dexam_exam_service:latest &
-docker pull eyadzz/dexam_user_service:latest &
-docker pull eyadzz/dexam_course_service:latest &
-docker pull eyadzz/dexam_api_gateway:latest &
-docker pull eyadzz/dexam_overseer_service:latest &
-docker-compose -f backend.yaml up --build
