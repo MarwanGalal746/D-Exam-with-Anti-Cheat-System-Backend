3ef06167139b2149857b1ae1bac048666442c8ec
Eyad Youssef <eyadyoussef1336@gmail.com>
2022-07-15 01:53:47 +0200

    added issuer to the token

--- src/main/java/com/DExam/User_Service/config/JwtManager.java
+++ src/main/java/com/DExam/User_Service/config/JwtManager.java
@@ -10,13 +10,14 @@ import java.util.Date;
 @Service
 public class JwtManager {
     private static final String SECRET_KEY = System.getenv("AUTH_SECRET_KEY");
+    private static final String issuer = "dexam-issuer";
     static byte[] secretKey = SECRET_KEY.getBytes();
     public static String generateToken(String email, String role) {
         Claims claims = Jwts.claims().setSubject(email);
         claims.put("UserRole", role);
         long expTime = System.currentTimeMillis() + 1000 * 60 * 60 * 10;
         Date exp = new Date(expTime);
-        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(exp)
+        return Jwts.builder().setClaims(claims).setIssuer(issuer).setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(exp)
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
 }
